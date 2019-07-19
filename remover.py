694aaf0e6538d46fca673b1d273d85f4c74e6bc8
rmcmill9 <rmcmill9@students.kennesaw.edu>
2019-07-18 21:06:02 -0400

    Update remover.py
    Created menu and added comments

--- remover.py
+++ remover.py
@@ -1,9 +1,11 @@
 # Import libraries we may need
 import sys, os, time, signal, webbrowser, platform, subprocess, pyfiglet
 
+#Use pyfiglet to make an easy ASCII logo
 greeting = pyfiglet.figlet_format("The Remover", font = "big")
 print(greeting)
 
+#Ask the user if they want to search by email or username
 def menu():
     print("1. Email \n2. Username \n")
 
