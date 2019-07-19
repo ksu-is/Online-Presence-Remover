40a9dd720bf02b4859bf7bc244e4e619cc84812e
rmcmill9 <rmcmill9@students.kennesaw.edu>
2019-07-18 20:50:13 -0400

    ASCII Greeting
    Created the ASCII greeting using the pyfiglet library

---
+++ remover.py
@@ -0,0 +1,5 @@
+# Import libraries we may need
+import sys, os, time, signal, webbrowser, platform, subprocess, pyfiglet
+
+greeting = pyfiglet.figlet_format("The Remover", font = "big")
+print(greeting)
