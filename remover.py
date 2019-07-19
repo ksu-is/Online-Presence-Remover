1010c47ba07e09b035ed75d5fb941036b6d7d5cd
rmcmill9 <rmcmill9@students.kennesaw.edu>
2019-07-18 21:01:55 -0400

    Selection Process
    Made selection process for user email and username

--- remover.py
+++ remover.py
@@ -3,3 +3,10 @@ import sys, os, time, signal, webbrowser, platform, subprocess, pyfiglet
 
 greeting = pyfiglet.figlet_format("The Remover", font = "big")
 print(greeting)
+
+def menu():
+    print("1. Email \n2. Username \n")
+
+    OPT = input("Please select which you'd like to search:")
+
+menu()
