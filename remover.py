f85a5dc778d116e806286e4e4deaa9f6f396b69a
rmcmill9 <rmcmill9@students.kennesaw.edu>
2019-07-24 22:32:01 -0400

    Update remover.py

--- remover.py
+++ remover.py
@@ -11,15 +11,12 @@ def menu():
 
     opt = input("Please type which you'd like to search (1 or 2) or enter '3' to exit:")
     if opt == "1":
-        os.system('cls')
         email()
     elif opt == "2":
-        os.system('cls')
         username()
     elif opt == "3":
         exit()
     else:
-        os.system('cls')
         print("Please enter a valid input \n")
         menu()
 
@@ -28,14 +25,12 @@ def menu():
 def email():
     mail = input("Please enter your email address or type 'r' to return: ")
     if mail == "r":
-        os.system('cls')
         menu()
     elif mail == (''):
         print("Invalid input")
         email()
     else:
         webbrowser.open('https://usersearch.org/results_email_basic.php?URL_email=' + mail)
-        os.system('cls')
         menu()
 
 
@@ -44,14 +39,12 @@ def email():
 def username():
     user = input("Please enter your username or type 'r' to return: ")
     if user == "r":
-        os.system('cls')
         menu()
     elif user == (''):
         print("Invalid input")
         user()
     else:
         webbrowser.open('https://usersearch.org/results_normal.php?URL_username=' + user)
-        os.system('cls')
         menu()
 
 menu()
