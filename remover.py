60a3b9102d86e6cf3037b69e2bf320ae973e25b9
rmcmill9 <rmcmill9@students.kennesaw.edu>
2019-07-24 21:08:39 -0400

    Update remover.py

--- remover.py
+++ remover.py
@@ -9,6 +9,26 @@ print(greeting)
 def menu():
     print("1. Email \n2. Username \n")
 
-    OPT = input("Please select which you'd like to search:")
+    opt = input("Please select which you'd like to search (1 or 2):")
+    if opt == "1":
+        email()
+    elif opt == "2":
+        username()
+    else:
+        print("Please enter a valid input \n")
+        opt()
 
+#Ask user for email
+
+def email():
+    mail = input("Please enter your email address: ")
+    webbrowser.open('https://usersearch.org/results_email_basic.php?URL_email=' + mail)
+    menu()
+
+#Ask user for username
+
+def username():
+    user = input("Please enter your username: ")
+    webbrowser.open('https://usersearch.org/results_normal.php?URL_username=' + user)
+    menu()
 menu()
