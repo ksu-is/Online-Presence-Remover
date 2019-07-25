ae73340a8ee926a0996b6342d94db043f21f4b7f
rmcmill9 <rmcmill9@students.kennesaw.edu>
2019-07-24 21:45:38 -0400

    Update remover.py

--- remover.py
+++ remover.py
@@ -7,28 +7,51 @@ print(greeting)
 
 #Ask the user if they want to search by email or username
 def menu():
-    print("1. Email \n2. Username \n")
+    print("1. Email \n2. Username \n3. Exit \n")
 
-    opt = input("Please select which you'd like to search (1 or 2):")
+    opt = input("Please type which you'd like to search (1 or 2) or enter '3' to exit:")
     if opt == "1":
+        os.system('cls')
         email()
     elif opt == "2":
+        os.system('cls')
         username()
+    elif opt == "3":
+        exit()
     else:
+        os.system('cls')
         print("Please enter a valid input \n")
-        opt()
+        menu()
 
 #Ask user for email
 
 def email():
-    mail = input("Please enter your email address: ")
-    webbrowser.open('https://usersearch.org/results_email_basic.php?URL_email=' + mail)
-    menu()
+    mail = input("Please enter your email address or type 'r' to return: ")
+    if mail == "r":
+        os.system('cls')
+        menu()
+    elif mail == (''):
+        print("Invalid input")
+        email()
+    else:
+        webbrowser.open('https://usersearch.org/results_email_basic.php?URL_email=' + mail)
+        os.system('cls')
+        menu()
+
 
 #Ask user for username
 
 def username():
-    user = input("Please enter your username: ")
-    webbrowser.open('https://usersearch.org/results_normal.php?URL_username=' + user)
-    menu()
+    user = input("Please enter your username or type 'r' to return: ")
+    if user == "r":
+        os.system('cls')
+        menu()
+    elif user == (''):
+        print("Invalid input")
+        user()
+    else:
+        webbrowser.open('https://usersearch.org/results_normal.php?URL_username=' + user)
+        os.system('cls')
+        menu()
+
 menu()
